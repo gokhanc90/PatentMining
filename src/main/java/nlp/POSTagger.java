5e8d6b76583c8e73466e58bbcbcb79657d72bfe6
ahmetaydin123 <ahmetaydin123@gmail.com>
2020-09-22 16:49:12 +0300

    noun referring adj çalışmaları

--- src/main/java/nlp/POSTagger.java
+++ src/main/java/nlp/POSTagger.java
@@ -3,8 +3,10 @@ package nlp;
 import edu.stanford.nlp.coref.data.CorefChain;
 import edu.stanford.nlp.ie.util.RelationTriple;
 import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.pipeline.*;
 import edu.stanford.nlp.semgraph.SemanticGraph;
+import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.trees.Tree;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -124,11 +126,8 @@ public class POSTagger {
         }
         writer.close();
 
-        System.out.println("***************************************************");
         Map<String,Integer> freq = new HashMap<>();
         for(Map.Entry<String,List<String>> entry : tags.entrySet()){
-            System.out.println(entry.getKey() + " : " + entry.getValue());
-
             List<String> words = entry.getValue();
 
             if(entry.getKey().startsWith("NN")){
@@ -141,7 +140,6 @@ public class POSTagger {
         Map<String,Integer> sortedByFreq =
                 freq.entrySet().stream()
                         .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-                        .limit(20)
                         .collect(Collectors.toMap(
                                 Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 
@@ -149,7 +147,7 @@ public class POSTagger {
             System.out.println(entry.getKey() + " - " + entry.getValue());
         }
 
-        
+
     }
 
     private void posTag(String text,BufferedWriter writer,int reviewNumber, HashMap<String,List<String>> tags) throws IOException {
@@ -188,7 +186,136 @@ public class POSTagger {
 
 
     public static void main(String[] args) throws IOException {
-        new POSTagger().process2();
+//        new POSTagger().process2();
+        String text = "5/35/18 update Don’t waste your money. " +
+                "These products are NOT made to last. " +
+                "The Robovac 11c replacement (3rd replacement) quit working properly. " +
+                "The vacuum now gives a false error as if there is something stuck. " +
+                "I spent $200 on a vacuum that lasted a year! " +
+                "We had a great initial experience with this product, but it was short lived. " +
+                "Unfortunately the warranty from the original purchase expired 60 days ago. " +
+                "I am now stuck with another Eufy Robovac that does not work, and all I get from customer support is 15% discount for another product that will not last. DON’T BUY IT!!!!! " +
+                "Update… Eufy customer support contacted me after my last review where I commented on the issues I was having after 3 months of continuous use. " +
+                "Once again, I have nothing but praises about their support. " +
+                "They offered another replacement, except this time with an 11c model. " +
+                "So far it works great, as did the first two. I hope this model lasts longer. " +
+                "I purchased this in march and was very pleased with it. " +
+                "The robovac does an excellent job navigating through our lower floor while we are asleep. " +
+                "It works quietly and we woke up to a clean floor. " +
+                "I am now on my second RoboVac thanks to Eufy's excellent customer service and running into the same problems I had with my original purchase. " +
+                "The vacuum does an excellent job avoiding walls and large objects, not so much when it comes to chair legs. " +
+                "After 3 months of ocasional bumping into chairs, the lens that covers the sensor gets scratched and becomes cloudy. " +
+                "Now I wake up to the sound of the robovac ramming into walls instead of avoiding them, just like my original did as well. " +
+                "I've tried cleaning the plastic lens but only works momentarily. " +
+                "Now it's also starting to have problems docking into the charging station, which I presume is due to the cloudy lens cover. " +
+                "It's sad that such a good product fails because o a small lens cover. " +
+                "Some small tweaks in design should fix this problem and allow the vacuum to work properly for longer than 3 months. " +
+                "Hey Eufy! How about placing a better rubber on the bumper to protect the lens cover, or changing the material ? " +
+                "If not for that this vac would have had a 5 star\n";
+
+
+        Properties props = new Properties();
+        // set the list of annotators to run
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma,ner,parse,depparse,coref,kbp,quote");
+        // set a property for an annotator, in this case the coref annotator is being set to use the neural algorithm
+        props.setProperty("coref.algorithm", "neural");
+        // build pipeline
+        StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+        // create a document object
+        CoreDocument document = new CoreDocument(text);
+        // annnotate the document
+        pipeline.annotate(document);
+
+
+        Map<String,List<String>> nouns = new LinkedHashMap<>();
+        for(CoreSentence sentence : document.sentences()) {
+            List<String> posTags = sentence.posTags();
+            for(int i=0;i<posTags.size(); i++){
+                String postag = posTags.get(i);
+                if(!postag.startsWith("NN")) continue;
+                nouns.put(sentence.tokens().get(i).word(),new ArrayList<>());
+
+            }
+        }
+
+
+
+        // second sentence
+//        CoreSentence sentence = document.sentences().get(5);
+
+        for(CoreSentence sentence : document.sentences()) {
+//        // list of the ner tags for the second sentence
+//        List<String> nerTags = sentence.nerTags();
+//        System.out.println("Example: ner tags");
+//        System.out.println(nerTags);
+//        System.out.println();
+//
+//        // constituency parse for the second sentence
+//        Tree constituencyParse = sentence.constituencyParse();
+//        System.out.println("Example: constituency parse");
+//        System.out.println(constituencyParse);
+//        System.out.println();
+
+            // dependency parse for the second sentence
+            SemanticGraph dependencyParse = sentence.dependencyParse();
+            System.out.println("Example: dependency parse");
+            System.out.println(dependencyParse);
+            System.out.println();
+
+
+            for(IndexedWord word : dependencyParse.vertexListSorted()){
+                for(SemanticGraphEdge edge : dependencyParse.getOutEdgesSorted(word)){
+                    System.out.println(edge.getTarget().word().split("/")[1]);
+
+                }
+            }
+            
+        }
+
+//        // kbp relations found in fifth sentence
+//        List<RelationTriple> relations =
+//                document.sentences().get(4).relations();
+//        System.out.println("Example: relation");
+//        System.out.println(relations.get(0));
+//        System.out.println();
+//
+//        // entity mentions in the second sentence
+//        List<CoreEntityMention> entityMentions = sentence.entityMentions();
+//        System.out.println("Example: entity mentions");
+//        System.out.println(entityMentions);
+//        System.out.println();
+//
+//        // coreference between entity mentions
+//        CoreEntityMention originalEntityMention = document.sentences().get(3).entityMentions().get(1);
+//        System.out.println("Example: original entity mention");
+//        System.out.println(originalEntityMention);
+//        System.out.println("Example: canonical entity mention");
+//        System.out.println(originalEntityMention.canonicalEntityMention().get());
+//        System.out.println();
+//
+//        // get document wide coref info
+//        Map<Integer, CorefChain> corefChains = document.corefChains();
+//        System.out.println("Example: coref chains for document");
+//        System.out.println(corefChains);
+//        System.out.println();
+//
+//        // get quotes in document
+//        List<CoreQuote> quotes = document.quotes();
+//        CoreQuote quote = quotes.get(0);
+//        System.out.println("Example: quote");
+//        System.out.println(quote);
+//        System.out.println();
+//
+//        // original speaker of quote
+//        // note that quote.speaker() returns an Optional
+//        System.out.println("Example: original speaker of quote");
+//        System.out.println(quote.speaker().get());
+//        System.out.println();
+//
+//        // canonical speaker of quote
+//        System.out.println("Example: canonical speaker of quote");
+//        System.out.println(quote.canonicalSpeaker().get());
+//        System.out.println();
 
     }
 }
