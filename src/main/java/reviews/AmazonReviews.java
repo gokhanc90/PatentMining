73c13e6133b1fd85ac180b0d66c4a5c6740d9286
gokhanc90 <gokhangoksel90@gmail.com>
2021-06-01 12:49:32 +0300

    useragent2

--- src/main/java/reviews/AmazonReviews.java
+++ src/main/java/reviews/AmazonReviews.java
@@ -79,7 +79,7 @@ public class AmazonReviews {
             for(int i=1; i<=e.getValue();i++) {
                 String url = baseUrl.replace("CURRENTPAGENUMBER",String.valueOf(i));
 
-                Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0").get();
+                Document doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0").get(); // Mozilla/5.0 (Macintosh; Intel Mac OS X x.y; rv:10.0) Gecko/20100101 Firefox/10.0
                 Element bodyH = doc.body();
                 Element cmReviewList = bodyH.getElementById("a-page");
                 Elements reviews = cmReviewList.getElementsByAttributeValue("data-hook","review");
